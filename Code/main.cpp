de8f1f6fba79e0e007cabcbb266ab2c146634547
lulek1410 <48333162+lulek1410@users.noreply.github.com>
2020-11-05 00:46:07 +0100

    jednak działa

--- Code/main.cpp
+++ Code/main.cpp
@@ -7,12 +7,9 @@ int main()
 {
 
 	sf::RenderWindow window(sf::VideoMode(1200, 800), "City Traffic Simulator", sf::Style::Close | sf::Style::Titlebar);
-	vector<Car> cars;
 	srand((unsigned)time(NULL));
 
-	for (int i = 0; i < 10; i++) {
-		cars.push_back(Car(rand() % 500 + 300, rand() % 500 + 300));
-	}
+	Car car(rand() % 500 + 200, rand() % 500 + 200);
 
 	while (window.isOpen())
 	{
@@ -22,12 +19,10 @@ int main()
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
-
-		for (int i = 0; i < 10; i++) {
-			window.draw(cars.at(i).getShape());
-			cars.at(i).move();
-		}
+	
 		window.clear();
+		window.draw(car.getShape());
+		car.move();
 		window.display();
 	}
 	return 0;
