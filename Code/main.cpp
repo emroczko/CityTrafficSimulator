40d36537b2795ca273056f299e1e792f1c6ba07a
Eryk <emroczko@me.com>
2020-11-04 13:02:37 +0100

    sfml

--- Code/main.cpp
+++ Code/main.cpp
@@ -9,7 +9,23 @@
 #include <SFML/Graphics.hpp>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+        sf::CircleShape shape(100.f);
+        shape.setFillColor(sf::Color::Blue);
+
+        while (window.isOpen())
+        {
+            sf::Event event;
+            while (window.pollEvent(event))
+            {
+                if (event.type == sf::Event::Closed)
+                    window.close();
+            }
+
+            window.clear();
+            window.draw(shape);
+            window.display();
+        }
+
+        return 0;
 }
