7d2c0fea4ba4276b4a8eaaf0533a4a3c7af5e301
Eryk <emroczko@me.com>
2020-11-04 01:49:13 +0100

    configure

---
+++ Code/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CityTrafficSimulator
+//
+//  Created by Eryk Mroczko on 04/11/2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
