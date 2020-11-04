3b03eb836d7024979b485bc0f0583ba95108a91b
lulek1410 <48333162+lulek1410@users.noreply.github.com>
2020-11-04 22:07:19 +0100

    dizła ale nic się nie dzieje

---
+++ Code/Vehicles/Car.h
@@ -0,0 +1,15 @@
+#include "SFML/Graphics.hpp"
+class Car
+{
+	private:
+		int x, y;
+		int speed;
+		sf::RectangleShape shape;
+		void updatePosition();
+
+	public:
+		Car(int x, int y);
+		~Car();
+		void move();
+		sf::RectangleShape getShape();
+};
--- Code/main.cpp
+++ Code/main.cpp
@@ -1,15 +1,35 @@
-//
-//  main.cpp
-//  CityTrafficSimulator
-//
-//  Created by Eryk Mroczko on 04/11/2020.
-//
-
-#include <iostream>
 #include <SFML/Graphics.hpp>
+#include "Vehicles/Car.h"
+#include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+int main()
+{
+	
+	sf::RenderWindow window(sf::VideoMode(1200, 800), "City Traffic Simulator", sf::Style::Close | sf::Style::Titlebar);
+	vector<Car> cars;
+	srand((unsigned)time(NULL));
+
+	for (int i = 0; i < 10; i++) {
+		cars.push_back(Car(rand()%500 + 300, rand() % 500 + 300));
+	}
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		for (int i = 0; i < 10; i++) {
+			window.draw(cars.at(i).getShape());
+			cars.at(i).move();
+		}
+		window.clear();
+		window.display();
+	}
+
+	return 0;
 }
