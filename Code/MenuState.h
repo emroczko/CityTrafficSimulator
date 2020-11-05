f740a105922fe02e550289af2b881a279eed04f6
Eryk <emroczko@me.com>
2020-11-05 13:48:16 +0100

    appEngine

--- Code/MenuState.h
+++ Code/MenuState.h
@@ -7,4 +7,4 @@ class MenuState{
     
     public:
     
-}
+};
--- Code/main.cpp
+++ Code/main.cpp
@@ -6,62 +6,7 @@
 using namespace std;
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(2160, 1680), "SFML WORK!");
-
-        Menu menu(window.getSize().x, window.getSize().y);
-
-        while (window.isOpen())
-        {
-            sf::Event event;
-
-            while (window.pollEvent(event))
-            {
-                switch (event.type)
-                {
-                case sf::Event::KeyReleased:
-                    switch (event.key.code)
-                    {
-                    case sf::Keyboard::Up:
-                        menu.MoveUp();
-                        break;
-
-                    case sf::Keyboard::Down:
-                        menu.MoveDown();
-                        break;
-
-                    case sf::Keyboard::Return:
-                        switch (menu.GetPressedItem())
-                        {
-                        case 0:
-                            std::cout << "Play button has been pressed" << std::endl;
-                            break;
-                        case 1:
-                            std::cout << "Option button has been pressed" << std::endl;
-                            break;
-                        case 2:
-                            window.close();
-                            break;
-                        }
-
-                        break;
-                    }
-
-                    break;
-                case sf::Event::Closed:
-                    window.close();
-
-                    break;
-
-                }
-            }
-
-            window.clear();
-
-            menu.draw(window);
-
-            window.display();
-        }
-	return 0;
+	return EXIT_SUCCESS;
 }
 
 
