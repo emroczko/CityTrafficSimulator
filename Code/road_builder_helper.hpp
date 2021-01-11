fec1c3e849968404f16bdd20994c82776adbb178
Eryk <emroczko@me.com>
2021-01-11 17:40:12 +0100

    Added roadBuilderHelper class

---
+++ Code/road_builder_helper.hpp
@@ -0,0 +1,32 @@
+//
+//  road_builder_helper.hpp
+//  CityTrafficSimulator
+//
+//  Created by Eryk Mroczko on 11/01/2021.
+//
+
+#include "SFML/Graphics.hpp"
+#include "simulator.hpp"
+#include "converter.hpp"
+
+
+namespace zpr {
+
+    class RoadBuilderHelper{
+    public:
+        RoadBuilderHelper(SimulatorDataRef data, int grid_size);
+        void checkRoadsTexture(std::vector<sf::RectangleShape>& roads);
+        
+    private:
+        void choseRoadWithOneNeighbour(sf::RectangleShape& road, std::shared_ptr<sf::RectangleShape> north, std::shared_ptr<sf::RectangleShape> south,
+            std::shared_ptr<sf::RectangleShape> east, std::shared_ptr<sf::RectangleShape> west, int row, int col);
+        void choseRoadWithTwoNeighbours(sf::RectangleShape& road, std::shared_ptr<sf::RectangleShape> north, std::shared_ptr<sf::RectangleShape> south,
+            std::shared_ptr<sf::RectangleShape> east, std::shared_ptr<sf::RectangleShape> west);
+        void choseRoadWithThreeNeighbours(sf::RectangleShape& road, std::shared_ptr<sf::RectangleShape> north, std::shared_ptr<sf::RectangleShape> south,
+            std::shared_ptr<sf::RectangleShape> east, std::shared_ptr<sf::RectangleShape> west);
+    
+        SimulatorDataRef data_;
+        std::unique_ptr<Converter> converter_;
+        int gridSize_;
+    };
+}
